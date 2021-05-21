88d2cc3923d8df67fe24cc1cbb062a02b558e15c
SamMacy <sam@senzing.com>
2021-05-21 12:27:09 -0400

    #32 add S3csv class

--- stream-producer.py
+++ stream-producer.py
@@ -1301,6 +1301,50 @@ class ReadWebsocketMixin():
         self.local_queue.close()
         self.local_queue.join_thread()
 
+# -----------------------------------------------------------------------------
+# Class: ReadWebsocketMixin
+# -----------------------------------------------------------------------------
+
+class ReadS3CsvMixin():
+
+    def __init__(self, config={}, *args, **kwargs):
+        logging.debug(message_debug(996, threading.current_thread().name, "ReadS3CsvMixin"))
+        self.input_url = config.get('input_url')
+        self.record_min = config.get('record_min')
+        self.record_max = config.get('record_max')
+        self.rows_in_chunk = config.get('csv_rows_in_chunk')
+        self.delimiter = config.get('csv_delimiter')
+        self.counter = 0
+        
+        #Instantiate boto3
+        
+        S3_client = boto3.client("S3")
+        
+        #Split S3 url
+        
+        self.urlParts = urlparse(self.input_url)
+        self.S3Bucket = self.urlParts.netloc
+        self.S3Key = self.urlParts.path.lstrip('/')
+        
+    def read(self):
+      self.response = S3_client.get_object(Bucket = self.S3Bucket, Key = self.S3Key)
+      
+      data = pandas.read_csv(self.response, skipinitialspace=True, dtype=str, chunksize=self.rows_in_chunk, delimiter=self.delimiter)
+        for data_frame in reader:
+            data_frame.fillna('', inplace=True)
+            for row in data_frame.to_dict(orient="records"):
+                # Remove items that have '' value
+                row = {i: j for i, j in row.items() if j != ''}
+
+                self.counter += 1
+                if self.record_min and self.counter < self.record_min:
+                    continue
+                if self.record_max and self.counter > self.record_max:
+                    break
+                assert type(row) == dict
+                yield row
+      
+      
 # =============================================================================
 # Mixins: Evaluate*
 #   Methods:
